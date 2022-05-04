2d99901318c009a26ec29a38dbe3edb6c3af6483
Joceline Marquez <andrea.marquezvivaldo@gmail.com>
2022-05-03 23:37:43 -0500

    Primer prueba, ok. El Rover está en el origen

---
+++ rover.js
@@ -0,0 +1,11 @@
+
+function rover (x, y, orientacion) {
+    //const x = 0;
+    //const y = 0;
+    //const orientacion = N;
+    if (x == 0 || y == 0 || orientacion == "N"){
+        return "origen"
+    }
+}
+
+module.exports = rover;
--- rover.test.js
+++ rover.test.js
@@ -1,5 +1,5 @@
 const rover = require('./rover');
 
 test ('Colocar el Rover en la posición inicial', () => {
-    expect(rover(0, 0, N)).toBe("origen");
+    expect(rover(0, 0, "N")).toBe("origen");
 });

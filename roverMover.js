a2610075ad6e50bbc2b5e72a867fccb23fcd5948
Joceline Marquez <andrea.marquezvivaldo@gmail.com>
2022-05-04 00:27:25 -0500

    El Rover también puede retroceder y no sólo ir al frente

--- roverMover.js
+++ roverMover.js
@@ -3,6 +3,10 @@ function roverMover (instrucciones){
 
         if (instrucciones == "F"){
             return "0, 1, N";
+        } else if (instrucciones == "B"){
+            return "0, 10, N";
+        } else {
+            return "Error";
         }
 }
 
--- roverMover.test.js
+++ roverMover.test.js
@@ -3,3 +3,7 @@ const roverMover = require('./roverMover');
 test ('Mover al Rover una casilla al frente', () => {
     expect(roverMover("F")).toBe("0, 1, N");
 });
+
+test ('Mover al Rover una casilla al frente', () => {
+    expect(roverMover("B")).toBe("0, 10, N");
+});

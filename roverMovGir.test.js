903727cdb2b6e83b9c322e73e7e9c8e37e0c723c
Joceline Marquez <andrea.marquezvivaldo@gmail.com>
2022-05-04 01:25:35 -0500

    Al agregar más instrucciones el Rover no supera la prueba

--- roverMovGir.test.js
+++ roverMovGir.test.js
@@ -3,3 +3,7 @@ const roverMovGir = require('./roverMovGir');
 test ('Mover al Rover hacía el frente y girar a la derecha', () => {
     expect(roverMovGir("FRF")).toBe(1, 1, "E");
 });
+
+test ('Mover al Rover hacía el frente y girar a la derecha y avanzar', () => {
+    expect(roverMovGir("FFRFF")).toBe(2, 2, "E");
+});
